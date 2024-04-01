18fa05a3cb4880783d7a95c948ee70b8dcca4b97
Nune Aghoyan <nuneaghoyan.flib@gmail.com>
2024-04-01 20:38:47 +0400

    Exchange

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,19 @@
 <script>
+import Exchange from './components/Exchange.vue';
 export default {
-  data() {
-    return {
-      email: ""
-    };
-  },
-  methods: {
-    emailInput() {
-      this.email = prompt("Enter your email:");
-    },
-    resetEmail() {
-      this.email = "";
-    },
-    validateEmail(userEmail) {
-      return /^[a-z0-9._-]+@[a-z]+\.[a-z]{2,4}$/.test(userEmail);
-    }
+  components: {
+    Exchange
   }
 }
+
 </script>
 
 <template>
-  <header>
-  </header>
 
   <main>
-
-    <button @click="emailInput()">Input Email</button>
-    <button @click="resetEmail()">Reset</button>
-    <div class="green-border" v-show="validateEmail(email)">
-      <h1>{{ email }}</h1>
-      <h4>The email is valid.</h4>
-    </div>
-    <div class="red-border" v-show="!validateEmail(email) && email">
-      <h1>{{ email }}</h1>
-      <h4>The email is invalid.</h4>
-    </div>
-
-    <!-- <div class="red-border" v-if="!validateEmail(email) && email">
-      <h1>{{ email }}</h1>
-      <h4>The email is invalid.</h4>
-    </div>
-    <div class="green-border" v-else-if="validateEmail(email) && email">
-      <h1>{{ email }}</h1>
-      <h4>The email is valid.</h4>
-    </div> -->
-
+    <Exchange />
   </main>
 
 </template>
 
-<style scoped>
-.green-border {
-  border: 2px solid green;
-  padding: 6px;
-  margin-top: 6px;
-  border-radius: 8px;
-}
-
-.red-border {
-  border: 2px solid red;
-  padding: 6px;
-  margin-top: 6px;
-  border-radius: 8px;
-}
-
-button {
-  margin-right: 10px;
-}
-
-header {
-  line-height: 1.5;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/Exchange.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      amount: 0,
+      currencyText: "AMD -> USD",
+      currencyBtn: "USD -> AMD"
+    };
+  },
+
+  methods: {
+    handleInputChange(event) {
+      this.count = event.target.value;
+      if (this.currencyText === "USD -> AMD") {
+        this.amount = this.count * 400;
+      } else {
+        this.amount = this.count / 400;
+      }
+    },
+
+    currencyChange() {
+      const amd = "AMD -> USD";
+      const usd = "USD -> AMD";
+
+      if (this.currencyBtn === usd) {
+        this.currencyBtn = amd;
+        this.currencyText = usd;
+
+      } else {
+        this.currencyText = amd;
+        this.currencyBtn = usd;
+      }
+
+      let count = this.count;
+      this.count = this.amount;
+      this.amount = count;
+
+      // this.count = 0;
+      // this.amount = 0;
+    },
+
+    // inputClick(event) {
+    //   this.count = this.count === 0 ? "" : event.target.value;
+    // }
+  }
+}
+</script>
+
+<template>
+  <h4>{{ currencyText }}</h4>
+  <input type="number" :value="count" @input="handleInputChange" @click="inputClick">
+  <input type="number" :value="amount" disabled>
+  <div><button @click="currencyChange">{{ currencyBtn }}</button></div>
+</template>
+
+<style scoped>
+h4 {
+  margin-left: 2px;
+}
+
+input {
+  margin-right: 2px;
+}
+</style>
